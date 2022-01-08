504025333e5c94b3a0f72317112fa990afc33987
ramc004 <calebrunner75@gmail.com>
2022-01-08 17:32:45 +0000

    Initial commit

---
+++ Mandelbrto/cmake-build-debug/CMakeFiles/Mandelbrto.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mandelbrto.dir/main.cpp.o"
+  "Mandelbrto.pdb"
+  "Mandelbrto"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mandelbrto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
