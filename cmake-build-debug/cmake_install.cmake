34e923cd494816b9c3bcab0575cd654802434484
ramc004 <calebrunner75@gmail.com>
2022-01-27 12:54:38 +0000

    s

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/CalebRam/Downloads/GitHub/Sixth Form/Repositries/CLionProjects
+# Install script for directory: /Users/CalebRam/Downloads/Programming/Sixth Form/Repositries/GitHub/CLionProjects
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/CalebRam/Downloads/GitHub/Sixth Form/Repositries/CLionProjects/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/CalebRam/Downloads/Programming/Sixth Form/Repositries/GitHub/CLionProjects/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
